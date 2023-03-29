ccb1ecef4ec85f34d4c806cd0e73ceb554c0f809
M Angeles Cordoba <mangeles.cg91@gmail.com>
2023-03-29 19:57:35 +0200

    Primeros módulos y componenetes

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
-
-}
+export class SidebarComponent {}
